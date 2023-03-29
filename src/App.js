c53354297c964d7f427c328d2103dbf7486e7cc6
Dae Heum Na <daeheumna@gmail.com>
2023-03-28 23:28:05 -0400

    quick hot fix to colors on select elements & font sizing

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <div className="wrapper">
         <header>
     
-            <h2>Random Recipe Generator</h2>
+            <h1>Random Recipe Generator</h1>
 
           <h5>Feeling unsure what to cook? Enter ingredient(s) below and we'll generate random recipes for you! For multiple ingredients, seperate them using commas.</h5>
         </header>
